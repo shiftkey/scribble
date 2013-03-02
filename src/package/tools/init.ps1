7da52b22deb3abc6ee36fbeec1a775af3cc2d23a
Brendan Forster <me@brendanforster.com>
2013-03-02 15:29:03 -0800

    refactoring to ensure init.ps1 doesn't clobber new files

--- src/package/tools/init.ps1
+++ src/package/tools/init.ps1
@@ -1,7 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
 function FindRootFolder {
-
     # worst case scenario, we dump this in the same folder as the solution file
     $solution_path = $dte.Solution.FullName
     $solution_dir = (get-item $solution_path).Directory.FullName
@@ -15,27 +14,28 @@ function FindRootFolder {
     $depth_from_tool_path = 5 # how deep should we go?
 
     do {
-	    $current_dir = $current_dir.parent
-        $parent_dir = $current_dir.FullName
-
-        if ([IO.Directory]::Exists($parent_dir) -eq $false)
+	    $current_dir = (Get-Item $current_dir).Parent.FullName
+        
+        if ([IO.Directory]::Exists($current_dir) -eq $false)
         {
-        	Write-Host "We've gone too high, bailing out"
+        	Write-Host "We can't go further, bailing out"
             break;
         }
 
-        if ([IO.Directory]::Exists((Join-Path $parent_dir ".git")))
+        if ([IO.Directory]::Exists((Join-Path -Path $current_dir ".git")))
         {
-            Write-Host "Found a .git folder at $parent_dir!"
-            $root_dir = $parent_dir
+            Write-Host "Found a .git folder at $current_dir!"
+            $root_dir = $current_dir
         }
 
-        if ([IO.Directory]::Exists((Join-Path $parent_dir ".hg")))
+        if ([IO.Directory]::Exists((Join-Path -Path $current_dir ".hg")))
         {
-            Write-Host "Found a .hg folder at $parent_dir!"
-            $root_dir = $parent_dir
+            Write-Host "Found a .hg folder at $current_dir!"
+            $root_dir = $current_dir
         }
 	
+        Write-Host "Nothing interesting found at $current_dir"
+
 	    $depth_from_tool_path--
     } while ($depth_from_tool_path -gt 0 -and $root_dir -eq $null)
 
@@ -50,12 +50,39 @@ function FindRootFolder {
 # get the "project" root folder
 $project_root = (FindRootFolder);
 $docs_folder = Join-Path -Path $project_root "docs"
+$metafile = Join-Path -Path $docs_folder ".duchess"
+$version = $package.Version.Version.ToString()
 
-$templatePath = Join-Path -Path $toolsPath "template\*"
+# if this is the first time we run the package
+if ([IO.Directory]::Exists($docs_folder) -eq $false) {
+	# create folder
+	$templatePath = Join-Path -Path $toolsPath "template\*"
 
-# populate folder with template contents
-Copy-Item -Path $templatePath -Destination $docs_folder -Recurse -Force
+	# populate folder with template contents
+	Copy-Item -Path $templatePath -Destination $docs_folder -Recurse
 
-# open the file in Visual Studio
-$index = Join-Path -Path $docs_folder "index.md"
-$dte.ItemOperations.OpenFile($index)
+	# dump the package version to a metafile
+	$version | Out-File $metafile
+
+	# open the file in Visual Studio
+	$index = Join-Path -Path $docs_folder "index.md"
+	$dte.ItemOperations.OpenFile($index)
+}
+else {
+	# if we have a different version installed, this is an upgrade
+	$installed_version = $null
+
+	if ([IO.File]::Exists($metafile) -eq $false) {
+		$installed_version = "i have no idea"
+	} else {
+		$installed_version = Get-Content $metafile | Out-String
+	}
+
+	if ($installed_version -ne $package) {
+		# update the version 
+		Remove-Item $metafile
+		$version | Out-File $metafile	
+	}
+}
+
+# TODO: hook in any other commands
