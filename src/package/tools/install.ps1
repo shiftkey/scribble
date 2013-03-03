7d4324bf4542a1262c5c89ce5ddd578b76c0b0fa
Brendan Forster <me@brendanforster.com>
2013-03-02 16:42:06 -0800

    removed scripts which are never run

--- src/package/tools/install.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-Write-Host "Well that's awesome, install.ps1 isn't available for solution-level packages"
--- src/package/tools/uninstall.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-Write-Host "Well that's awesome, uninstall.ps1 isn't available for solution-level packages"
