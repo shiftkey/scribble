fc24dfa1b1a2da4ad07e3da2fcc992227f6e307a
Brendan Forster <me@brendanforster.com>
2013-04-07 11:51:40 +0930

    and more refactoring of tests

--- src/Scribble.CodeSnippets/Scribble.CodeSnippets.Tests/ValidationTests.cs
+++ src/Scribble.CodeSnippets/Scribble.CodeSnippets.Tests/ValidationTests.cs
@@ -8,7 +8,7 @@ namespace Scribble.CodeSnippet.Tests
     public class ValidationTests
     {
         [Fact(Skip = "Todo")]
-        public void Display_Message_When_Tag_Found_In_Docs_But_Not_Found_In_Code()
+        public void When_Tag_Found_In_Docs_But_Not_Found_In_Code_Returns_False()
         {
             var directory = @"data\validation\site-no-snippets\".ToCurrentDirectory();
 
@@ -18,20 +18,35 @@ namespace Scribble.CodeSnippet.Tests
             var result = Importer.Update(codeFolder, new[] { "*.cs" }, docsFolder);
 
             Assert.False(result.Completed);
-
-            var error = result.Errors.First();
-            var message = error.Message;
-
-            Assert.Equal(message, "Could not find a code snippet for reference 'LinqToJsonCreateParse'");
-            // TODO: debug mode?
-            // TODO: i18n?
         }
 
+        [Fact(Skip = "Todo")]
+        public void When_Tag_Found_In_Docs_But_Not_Found_In_Code_Display_Message()
+        {
+            var directory = @"data\validation\site-no-snippets\".ToCurrentDirectory();
+
+            var codeFolder = Path.Combine(directory, @"source\");
+            var docsFolder = Path.Combine(directory, @"docs\");
+
+            var result = Importer.Update(codeFolder, new[] { "*.cs" }, docsFolder);
+
+            var error = result.Errors.First();
+
+            // message explains error
+            Assert.Equal(error.Message, "Could not find a code snippet for reference 'LinqToJsonCreateParse'");
+
+            // file is as we expected
+            Assert.True(error.File.StartsWith(codeFolder));
+            Assert.True(error.File.EndsWith("code.cs"));
+
+            // and we have the right line number to look at
+            Assert.Equal(error.LineNumber, 30);
+        }
 
         [Fact]
         public void When_Code_Snippet_Defined_But_Not_Used_Returns_True()
         {
-            var directory = @"data\validation\site-no-reference\".ToCurrentDirectory();
+            var directory = @"data\validation\no-reference\".ToCurrentDirectory();
 
             var codeFolder = Path.Combine(directory, @"source\");
             var docsFolder = Path.Combine(directory, @"docs\");
@@ -44,7 +59,7 @@ namespace Scribble.CodeSnippet.Tests
         [Fact(Skip = "Todo")]
         public void When_Code_Snippet_Defined_But_Not_Used_In_Docs_Displays_Warning_Message()
         {
-            var directory = @"data\validation\site-no-reference\".ToCurrentDirectory();
+            var directory = @"data\validation\no-reference\".ToCurrentDirectory();
 
             var codeFolder = Path.Combine(directory, @"source\");
             var docsFolder = Path.Combine(directory, @"docs\");
@@ -52,10 +67,15 @@ namespace Scribble.CodeSnippet.Tests
             var result = Importer.Update(codeFolder, new[] { "*.cs" }, docsFolder);
 
             var error = result.Errors.First();
-            var message = error.Message;
-            Assert.Equal(message, "Code snippet reference 'LinqToJsonCreateParse' is not used in any pages and can be removed");
-            // TODO: include file path? line?
-            // TODO: debug mode?
+            // message explains error
+            Assert.Equal(error.Message, "Code snippet reference 'LinqToJsonCreateParse' is not used in any pages and can be removed");
+            
+            // file is as we expected
+            Assert.True(error.File.StartsWith(codeFolder));
+            Assert.True(error.File.EndsWith("code.cs"));
+
+            // and we have the right line number to look at
+            Assert.Equal(error.LineNumber, 30);
         }
 
         [Fact]
