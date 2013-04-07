8434a2aef55ca9d4be21b7a998a64ef9f799c733
Brendan Forster <me@brendanforster.com>
2013-04-07 10:58:14 +0930

    unneeded using statements

--- src/Scribble.CodeSnippets/Scribble.CodeSnippets.Tests/CodeFileParserTests.cs
+++ src/Scribble.CodeSnippets/Scribble.CodeSnippets.Tests/CodeFileParserTests.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Reflection;
 using Scribble.CodeSnippets;
 using Xunit;
 
--- src/Scribble.CodeSnippets/Scribble.CodeSnippets.Tests/ValidationTests.cs
+++ src/Scribble.CodeSnippets/Scribble.CodeSnippets.Tests/ValidationTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
+﻿using System.IO;
 using Scribble.CodeSnippets;
 using Xunit;
 
