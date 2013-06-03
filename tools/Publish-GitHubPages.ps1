058614e8d6728ac5ff5021ad6957e1445cd42d5f
Brendan Forster <me@brendanforster.com>
2013-06-03 23:05:00 +1000

    90% of the way there, just needs better parsing of remotes

--- tools/Publish-GitHubPages.ps1
+++ tools/Publish-GitHubPages.ps1
@@ -1,5 +1,12 @@
 param([switch]$PushToRemote)
 
+$scriptpath = $MyInvocation.MyCommand.Path
+$toolsPath = Split-Path $scriptpath
+$root_dir = Split-Path $toolsPath
+Write-host "My directory is $root_dir"
+
+$assemblyPath = Join-Path $toolsPath "libgit2sharp\LibGit2Sharp.dll"
+[void][Reflection.Assembly]::LoadFile($assemblyPath)
 
 function Update-GithubPages($current_repository, $base_url) {
 
@@ -17,8 +24,9 @@ function Update-GithubPages($current_repository, $base_url) {
 
 	Set-Location -Path $repoPath
 
-	. git clone -b gh-pages -o upstream $current_repository . | Out-Null
-
+    # TODO: can we just recreate the gh-pages branch without needing a full clone?
+	$repo = [LibGit2Sharp.Repository]::Clone($current_repository, $repoPath)
+	$branch = $repo.Checkout("origin/gh-pages", [LibGit2Sharp.CheckoutOptions]::None, $null)
 	"Replacing contents with generated content"
 
 	Remove-Item * -Exclude .git -Recurse -Force
@@ -29,10 +37,16 @@ function Update-GithubPages($current_repository, $base_url) {
     $template = Get-Content "_config.yml"
     $template -replace "baseurl: /", "baseurl: $base_url" | Out-File "_config.yml" -encoding "ASCII"
 
-	. git add . | Out-Null
-	. git add -u . | Out-Null
-	. git commit -m "updating site from scribble generated content" | Out-Null
-	. git push upstream gh-pages | Out-Null
+    $repo.Index.Stage("*");
+    $args = @("scribble", "scribble@nowhere.com", [System.DateTimeOffset]::Now)
+    $signature = New-Object -Type LibGit2Sharp.Signature -ArgumentList $args
+    $repo.Commit("updating site from scribble generated content", $signature, $signature) | Out-Null
+       
+    Write-Host $branch.Remote.Name
+    # TODO: we need this to work against a proper remote
+    $repo.Network.Push($branch.Remote, $branch.CanonicalName, $null)
+
+	$repo.Dispose()
 
 	Set-Location -Path $currentLocation
 
@@ -44,5 +58,6 @@ Update-GithubPages "D:\Code\github\shiftkey\scribble\" "/scribble/"
 
 if ($PushToRemote) { 
 	"Pushing to GitHub now"
-    . git push origin gh-pages --force
+	# TODO: push this
+    # . git push origin gh-pages --force
 }
