7f679725a7812970539d5adcd930fae809a992b9
Brendan Forster <me@brendanforster.com>
2013-06-12 12:51:56 +1000

    tweaking build script

--- package.ps1
+++ package.ps1
@@ -4,21 +4,23 @@ function Get-ScriptDirectory
 	Split-Path $Invocation.MyCommand.Path
 }
 
-$devFolder = "D:\Code\packages"
+$devFolder = "D:\packages"
 $devPackages = Get-ChildItem $devFolder | Where {$_.Name -like "scribble*" }
 
 if ($devPackages.length -eq 0) {
 	$Version = "0.4.3.1000"
+} else {
+	$lastPackage = $devPackages[-1].Name
+	$groups = ([regex]"\d+").matches($lastPackage).groups
+	$build_number = $groups[-1].value
+	$new_build_number = 1 + [int]$build_number
+
+	$Version = $lastPackage -replace $build_number, $new_build_number `
+						    -replace "scribble.", "" `
+						    -replace "-pre.nupkg", ""
 }
 
-$lastPackage = $devPackages[-1].Name
-$groups = ([regex]"\d+").matches($lastPackage).groups
-$build_number = $groups[-1].value
-$new_build_number = 1 + [int]$build_number
 
-$Version = $lastPackage -replace $build_number, $new_build_number `
-					    -replace "scribble.", "" `
-					    -replace "-pre.nupkg", ""
 "Using new version number $Version"
 
 powershell -File build.ps1 -Version $Version
